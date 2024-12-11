289a02f040ac2a00a2ee1e94b889b5f5366bf538
Muhammad Imam arif darmawan <muhamadimamarif225@gmail.com>
2024-12-11 20:18:49 +0700

    feat: add footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import Navbar from "../Components/Navbar"
+import Footer from "../Components/Footer"
 import '../styles/Home.css'
+
 function Home() {
   return (
     <div>
         <Navbar />
+        <Footer />
     </div>
   )
 }
