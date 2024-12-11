ee532ed2adf154e534dac7428fef482a7163c59c
Muhammad Imam arif darmawan <muhamadimamarif225@gmail.com>
2024-12-11 21:42:06 +0700

    feat : add Tutors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import Navbar from "../Components/Navbar"
 import Footer from "../Components/Footer"
 import { homeSection } from "../data/HomeSection"
 import { courseSection } from "../data/CourseSection"
+import { tutorsSection, tutorList } from "../data/TutorsSection"
+import Tutors from "../Components/Tutors"
 import '../styles/Home.css'
 
 import parse from 'html-react-parser'
@@ -22,6 +24,14 @@ function Home() {
                 {parse(courseSection.content)}
             </div>
             <img src={courseSection.image}/>
+        </section>
+        <section id="tutors">
+            <div className="tengah">
+                <div className="kolom">
+                    {parse(tutorsSection.content)}
+                </div>
+                <Tutors tutorList={tutorList} />
+            </div>
         </section>
         </div>
         <Footer />
