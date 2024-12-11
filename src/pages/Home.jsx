858035ed7bbabca3075131e7e1f40ef5bced1070
Muhammad Imam arif darmawan <muhamadimamarif225@gmail.com>
2024-12-11 20:35:26 +0700

    feat add HomeSection and implement html-react-parse

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,22 @@
 import Navbar from "../Components/Navbar"
 import Footer from "../Components/Footer"
+import { homeSection } from "../data/HomeSection"
 import '../styles/Home.css'
+
+import parse from 'html-react-parser'
 
 function Home() {
   return (
     <div>
         <Navbar />
+        <div className="wrapper">
+        <section id="home">
+            <img src={homeSection.image} />
+            <div className="kolom">
+                {parse(homeSection.content)}
+            </div>
+        </section>
+        </div>
         <Footer />
     </div>
   )
