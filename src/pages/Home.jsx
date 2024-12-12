5f3188ce8e9474faa75dbd0525a3d922b7c4611c
Muhammad Imam arif darmawan <muhamadimamarif225@gmail.com>
2024-12-12 11:35:37 +0700

    feat: Partners

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,9 @@ import Footer from "../Components/Footer"
 import { homeSection } from "../data/HomeSection"
 import { courseSection } from "../data/CourseSection"
 import { tutorsSection, tutorList } from "../data/TutorsSection"
+import { partnersSection, partnerList } from "../data/PartnersSection"
 import Tutors from "../Components/Tutors"
+import Partners from "../Components/Partners"
 import '../styles/Home.css'
 
 import parse from 'html-react-parser'
@@ -31,6 +33,14 @@ function Home() {
                     {parse(tutorsSection.content)}
                 </div>
                 <Tutors tutorList={tutorList} />
+            </div>
+        </section>
+        <section id="partners">
+            <div className="tengah">
+                <div className="kolom">
+                    {parse(partnersSection.content)}
+                </div>
+                <Partners partnerList={partnerList} />
             </div>
         </section>
         </div>
